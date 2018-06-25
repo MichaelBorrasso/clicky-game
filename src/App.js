627ef4ef3565ea87fd0eb44b4f2eda1ea09e7adb
MichaelBorrasso <michaelborrasso@gmail.com>
2018-06-25 16:13:50 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ShapeCard from "./components/ShapeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import shapes from "./shapes.json";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    shapes
+  };
+
+  moveShape = id => {
+    // Filter this.state.shapes for shape with an id not equal to the id being clicked
+    const shapes = this.state.shapess.filter(shape => shape.id !== id);
+    // Set this.state.shapes equal to the new shapes array
+    this.setState({ shapes });
+  };
+
+  // Map over this.state.shapes and render a ShapeCard component for each shape object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">clicky-game</h1>
-        </header>
-        <p className="App-intro">
-        
-        </p>
-      </div>
+      <Wrapper>
+        <Title>Shapes</Title>
+        {this.state.shapes.map(shape => (
+          <ShapeCard
+            moveShape={this.moveShape}
+            id={shape.id}
+            key={shape.id}
+            name={shape.name}
+            image={shape.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
